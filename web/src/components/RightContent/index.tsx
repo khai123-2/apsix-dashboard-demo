87a8196e0cb95d453020476024ade0f6f6e242be
khai123-2 <tranquangkhai0910@gmail.com>
2022-05-14 12:14:15 +0700

    edit demo

--- web/src/components/RightContent/index.tsx
+++ web/src/components/RightContent/index.tsx
@@ -21,6 +21,7 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
 */
+import { Tag, Space } from 'antd';
 import React from 'react';
 import { useModel, SelectLang } from 'umi';
 
